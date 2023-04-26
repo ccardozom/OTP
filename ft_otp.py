09dfcaf54e4d9f1630ec2fd8c48d411ebd694fc7
ccardozom <carloscardozom@gmail.com>
2023-04-26 11:28:41 +0200

    parser arguments is ok

--- ft_otp.py
+++ ft_otp.py
@@ -2,7 +2,11 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Generador de contraseñas a partir de un hexadecimal")
 parser.add_argument("hex_key", help="Clave hexadecimal de al menos 64 caracteres")
-parser.add_argument("-g", dest="save_hex_key", help="Guarda la clave hexadecimal en un archivo llamado ft_opt.key")
-parser.add_argument("-k", dest="new_key", help="Genera una nueva contraseña temporal")
+parser.add_argument("-g", dest="save_hex_key", action="store_true" , help="Guarda la clave hexadecimal en un archivo llamado ft_opt.key")
+parser.add_argument("-k", dest="new_key", action="store_true", help="Genera una nueva contraseña temporal")
 
 params = parser.parse_args()
+
+print(params.hex_key)
+print(params.save_hex_key)
+print(params.new_key)

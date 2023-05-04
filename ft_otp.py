29f2d91640b405fc8861b95bb4593a2f831e6d61
ccardozom <carloscardozom@gmail.com>
2023-05-04 17:04:55 +0200

    todo ok

--- ft_otp.py
+++ ft_otp.py
@@ -11,6 +11,7 @@ import time
 import math
 import re
 from cryptography.fernet import Fernet
+import qrcode_terminal
 
 regex_hex = re.compile(r'^[0-9a-fA-F]+$')
 clave = b'nJg9XY8gSXYRtCUOqvoVfgQD7NfEpBSTWF-K8OjwjVY='
@@ -28,6 +29,7 @@ def int_a_byte(numero, longitud):
     """Convierte un número entero en una cadena de bytes con una longitud específica"""
     return numero.to_bytes(longitud, byteorder='big')
 
+
 def byte_a_int(cadena_bytes):
     """Convierte una cadena de bytes en un número entero"""
     return int.from_bytes(cadena_bytes, byteorder='big')
@@ -36,19 +38,14 @@ def generar_valor_totp(clave_secreta):
     """Genera un valor TOTP a partir de una clave secreta"""
     ts_number = math.floor(time.time() / 30)
     clave = clave_secreta.encode()
+
     mensaje = int_a_byte(ts_number, 8)
+
     valor_hmac = hmac.new(clave, mensaje, hashlib.sha1).digest()
+
     offset = (valor_hmac[-1]) & 0xf
     key_of_set = byte_a_int(valor_hmac[offset:offset+4]) & 0x7fffffff
     final_key = '{:06d}'.format(key_of_set % 10 ** 6)
-    ''' print()
-    print(f"ts_number: {ts_number}")
-    print(f"clave: {clave}")
-    print(f"mensaje: {mensaje}")
-    print(f"valor_hmac: {valor_hmac}")
-    print(f"offset: {offset}")
-    print(f"key_of_set: {key_of_set}")
-    print(f"final_key: {final_key}") '''
     
     return final_key
 
@@ -57,15 +54,14 @@ if __name__ == '__main__':
         hex_key = ""
         try:
             with open(param1, 'r') as f:
-                hex_key = f.read()
+                hex_key = f.readline()
+            f.close()
         except:
             hex_key = param1
         if regex_hex.match(hex_key ) and len(hex_key) % 2 == 0 and len(hex_key) >= 64:
-            print(hex_key)
             f = Fernet(clave)
             encriptado = f.encrypt(hex_key.encode())
-            print(encriptado)
-            with open("ft_opt.key","wb") as file:
+            with open("ft_otp.key","wb") as file:
                 file.write(encriptado)
             print("Se ha generado el archivo encriptado correctamente.")
         else:
@@ -78,8 +74,13 @@ if __name__ == '__main__':
             hex_key = f.decrypt(hex_key).decode()
             key_decode = bytes.fromhex(hex_key)
             clave_secreta = key_decode.decode() # reemplazar por tu propia clave secreta
-            print(f"clave_secreta: {clave_secreta}")
             valor_totp = generar_valor_totp(clave_secreta)
             print('Tu código TOTP es:', valor_totp)
+            try:
+                qrcode_terminal.draw(str(valor_totp))
+            except:
+                print("El QR de los cojones ha fallado")
         except:
             print("No se puede leer y generar la clave")
+    else:
+        print("Error: unrecognized flag")
